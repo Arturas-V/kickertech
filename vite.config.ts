f4360f672015ce21062be2df6f01a7e6308d00ab
Arturas-V <larytis@gmail.com>
2026-01-14 14:44:27 +0200

    Country flags added, removing already added team(Eurobasket)

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: '/kickertech/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
